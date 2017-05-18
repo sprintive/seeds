083448929bf22c0f6f89e53b077235de831513bc
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2017-05-18 19:22:32 +0300

    Issue #2879481: Fix seeds layout name and add card layout

--- modules/custom/seeds_pollination/src/Plugin/Layout/SeedsLighthouse.php
+++ modules/custom/seeds_pollination/src/Plugin/Layout/SeedsLighthouse.php
@@ -3,20 +3,37 @@
 namespace Drupal\seeds_pollination\Plugin\Layout;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Plugin\PluginFormInterface;
 use Drupal\Core\Layout\LayoutDefault;
 
 /**
+ * Ne
  * Layout class for all Foundation layouts.
  */
-class SeedsHighlighted extends LayoutDefault {
+class SeedsLighthouse extends LayoutDefault implements PluginFormInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
 
   /**
    * {@inheritdoc}
    */
   public function defaultConfiguration() {
     return parent::defaultConfiguration() + [
-      'wrappers' => [],
       'wrapper_classes' => '',
+      'wrapper_id' => '',
+      'row_1' => 'container',
+      'row_2' => 'container',
+      'row_4' => 'container',
+      'row_7' => 'container',
+      'row_9' => 'container',
+      'row_12' => 'container',
+      'row_14' => 'container',
+      'row_15' => 'container',
     ];
   }
 
@@ -24,14 +41,13 @@ class SeedsHighlighted extends LayoutDefault {
    * {@inheritdoc}
    */
   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildConfigurationForm($form, $form_state);
+
     $configuration = $this->getConfiguration();
 
     $form['attributes'] = [
       '#group' => 'additional_settings',
       '#type' => 'details',
       '#title' => $this->t('Wrapper attributes'),
-      '#description' => $this->t('Attributes for the outermost element'),
       '#tree' => TRUE,
     ];
 
@@ -51,6 +67,20 @@ class SeedsHighlighted extends LayoutDefault {
       '#weight' => 1,
     ];
 
+    $container_classes = [
+      'container' => 'container',
+      'fluid-container' => 'fluid-container',
+    ];
+
+    foreach ($this->getRows() as $row) {
+      $form['attributes'][$row] = [
+        '#type' => 'select',
+        '#title' => $this->t(ucfirst(str_replace('_', ' ', $row))),
+        '#options' => $container_classes,
+        '#default_value' => $configuration[$row],
+      ];
+    }
+
     return $form;
   }
 
@@ -59,10 +89,27 @@ class SeedsHighlighted extends LayoutDefault {
    */
   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
     $this->configuration['attributes'] = $form_state->getValue('attributes');
-    foreach (['wrapper_classes', 'wrapper_id'] as $name) {
+
+    $classes = array_merge(['wrapper_id', 'wrapper_classes'], $this->getRows());
+
+    foreach ($classes as $name) {
       $this->configuration[$name] = $this->configuration['attributes'][$name];
       unset($this->configuration['attributes'][$name]);
     }
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $form_state) {
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getRows() {
+    return ['row_1', 'row_2', 'row_4', 'row_7', 'row_9', 'row_12', 'row_14', 'row_15'];
   }
 
 }
