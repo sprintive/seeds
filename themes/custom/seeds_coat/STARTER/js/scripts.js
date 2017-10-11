4086eed05ed2b8b12aa1ee3543c4321fc9a0e838
Mohammad AlQanneh <mqanneh@gmail.com>
2017-10-11 12:12:59 +0300

    Added default theme js file and included from theme libraries.

---
+++ themes/custom/seeds_coat/STARTER/js/scripts.js
@@ -0,0 +1,14 @@
+/**
+ * @file Behaviors for the THEMENAME theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.BEHAVIOURNAME = {
+    attach: function (context, settings) {
+
+    }
+  }
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
