66a389061f6402bfcddf66bec595d2f9596c969a
mqanneh <mqanneh@2833163.no-reply.drupal.org>
2018-03-07 20:21:59 +0200

    Issue #2950929 by mqanneh: Media browser: selected items loses selected class after submission

--- modules/embryos/seeds_media/js/seeds_media.js
+++ modules/embryos/seeds_media/js/seeds_media.js
@@ -12,11 +12,15 @@
    */
   Drupal.behaviors.seedsMedia = {
     attach: function (context, settings) {
-      $('.view-media-browser .views-col', context).once().click(function () {
-        var $row = $(this);
-        var $input = $row.find('.views-field-entity-browser-select input');
-        $input.prop('checked', !$input.prop('checked'));
-        $row[$input.prop('checked') ? 'addClass' : 'removeClass']('checked');
+      $('.view-media-browser .views-col').once().each(function () {
+        var input = $(this).find('.views-field-entity-browser-select input');
+        (input.prop('checked') === true) ? $(this).addClass('checked') : $(this).removeClass('checked');
+      });
+
+      $('.view-media-browser .views-col', context).click(function () {
+        var input = $(this).find('.views-field-entity-browser-select input');
+        input.prop('checked', !input.prop('checked'));
+        $(this)[input.prop('checked') ? 'addClass' : 'removeClass']('checked');
       });
     }
   };
