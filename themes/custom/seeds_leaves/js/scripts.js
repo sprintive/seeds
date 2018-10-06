c556f1f73954f24f3f0198dd8c663585d3de9484
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2018-10-06 19:26:23 +0300

    Issue #2998362 by m.abdulqader: Add Seeds Leaves theme which built on Bootstrap SASS

---
+++ themes/custom/seeds_leaves/js/scripts.js
@@ -0,0 +1,11 @@
+/**
+ * @file
+ * Seeds Coat custom JS. 
+ */
+(function ($, Drupal, Bootstrap) {
+  Drupal.behaviors.seeds_leaves = {
+    attach: function (context) {
+
+    }
+  };
+})(window.jQuery, window.Drupal, window.Drupal.bootstrap);
