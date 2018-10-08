7bcf27a257a768cfbeea28079a0b1531add02b14
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2018-10-08 18:44:11 +0300

    Issue #2998362 by m.abdulqader: Fix entity browser search

--- modules/embryos/seeds_media/js/seeds_media.js
+++ modules/embryos/seeds_media/js/seeds_media.js
@@ -1,8 +1,3 @@
-/**
- * @file
- * Defines the behavior of the media entity browser view.
- */
-
 (function ($) {
 
   "use strict";
@@ -16,12 +11,17 @@
         var input = $(this).find('.views-field-entity-browser-select input');
         (input.prop('checked') === true) ? $(this).addClass('checked') : $(this).removeClass('checked');
       });
-
-      $('.view-media-browser .views-col', context).click(function () {
-        var input = $(this).find('.views-field-entity-browser-select input');
-        input.prop('checked', !input.prop('checked'));
-        $(this)[input.prop('checked') ? 'addClass' : 'removeClass']('checked');
-      });
+      function bindButtonClick() {
+        $('.view-media-browser .views-col', context).click(function () {
+          var input = $(this).find('.views-field-entity-browser-select input');
+          input.prop('checked', !input.prop('checked'));
+          $(this)[input.prop('checked') ? 'addClass' : 'removeClass']('checked');
+        });
+      }
+      $(document).ajaxStop(function () {
+        bindButtonClick();
+      })
+      bindButtonClick()
     }
   };
 
