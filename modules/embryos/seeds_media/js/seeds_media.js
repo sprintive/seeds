823966c56c7e8b2232b9cd148b197ac96b8605a6
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2018-12-08 18:01:08 +0200

    Issue #3018737: Entity Browser select not working after serch

--- modules/embryos/seeds_media/js/seeds_media.js
+++ modules/embryos/seeds_media/js/seeds_media.js
@@ -1,27 +1,27 @@
 (function ($) {
-
+  let initialized = false;
   "use strict";
 
   /**
    * Attaches the behavior of the media entity browser view.
    */
+
+  function bindButtonClick(context) {
+
+    if ($(context).is('form')) {
+      return;
+    }
+
+    $('.views-view-grid .views-col', $(context)).click(function () {
+      var input = $(this).find('.views-field-entity-browser-select input');
+      input.prop('checked', !input.prop('checked'));
+      $(this)[input.prop('checked') ? 'addClass' : 'removeClass']('checked');
+    });
+  }
+
   Drupal.behaviors.seedsMedia = {
     attach: function (context, settings) {
-      $('.view-media-browser .views-col').once().each(function () {
-        var input = $(this).find('.views-field-entity-browser-select input');
-        (input.prop('checked') === true) ? $(this).addClass('checked') : $(this).removeClass('checked');
-      });
-      function bindButtonClick() {
-        $('.view-media-browser .views-col', context).click(function () {
-          var input = $(this).find('.views-field-entity-browser-select input');
-          input.prop('checked', !input.prop('checked'));
-          $(this)[input.prop('checked') ? 'addClass' : 'removeClass']('checked');
-        });
-      }
-      $(document).ajaxStop(function () {
-        bindButtonClick();
-      })
-      bindButtonClick()
+      bindButtonClick(context);
     }
   };
 
