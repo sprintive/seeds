2539b5db88ba6b7fba5a726e063ebdb766b6c7ec
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2019-07-10 17:52:15 +0300

    Issue #3062026: Create New Subtheme that depend on BS4

---
+++ themes/custom/seeds_coat/subtheme/gulpfile.js
@@ -0,0 +1,96 @@
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  cleanCss = require("gulp-clean-css"),
+  autoprefixer = require("gulp-autoprefixer"),
+  sourcemaps = require("gulp-sourcemaps"),
+  livereload = require("gulp-livereload"),
+  rtlcss = require('gulp-rtlcss'),
+  rename = require('gulp-rename'),
+  del = require("del");
+
+gulp.task(
+  "sass",
+  gulp.series(function(done) {
+    gulp
+      .src(["./scss/style.scss"])
+      .pipe(sourcemaps.init())
+      .pipe(
+        sass({
+          includePaths: [
+            "node_modules/font-awesome-sass/assets/stylesheets/"
+          ]
+        }).on("error", sass.logError)
+      )
+      .pipe(
+        autoprefixer({
+          Browserslist: [
+            "Chrome >= 35",
+            "Firefox >= 38",
+            "Edge >= 12",
+            "Explorer >= 10",
+            "iOS >= 8",
+            "Safari >= 8",
+            "Android 2.3",
+            "Android >= 4",
+            "Opera >= 12"
+          ]
+        })
+      )
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("./css"))
+      .pipe(livereload());
+
+      gulp
+      .src(["./scss/style.scss"])
+      .pipe(sourcemaps.init())
+      .pipe(
+        sass({
+          includePaths: [
+            "node_modules/font-awesome-sass/assets/stylesheets/"
+          ]
+        }).on("error", sass.logError)
+      )
+      .pipe(rtlcss()) // Convert to RTL.
+      .pipe(rename({ suffix: '-rtl' })) // Append "-rtl" to the filename.
+			.pipe(sourcemaps.write('./')) // Output source maps.
+			.pipe(cleanCss())
+			.pipe(gulp.dest('./css')) // Output RTL stylesheets.
+      .pipe(livereload());
+    done();
+  })
+);
+gulp.task(
+  "copy",
+  gulp.series(function(done) {
+    gulp
+      .src([
+        "node_modules/font-awesome-sass/assets/fonts/font-awesome/fontawesome-webfont.*"
+      ])
+      .pipe(gulp.dest("fonts/font-awesome/"));
+    done();
+  })
+);
+
+gulp.task(
+  "clean",
+  gulp.series(function(done) {
+    del(["fonts/font-awesome", "fonts/bootstrap", "css"]);
+    done();
+  })
+);
+
+gulp.task(
+  "build",
+  gulp.series("clean","copy","sass", function(done) {
+    done();
+  })
+);
+
+gulp.task(
+  "watch",
+  gulp.series("copy", "sass", function(done) {
+    livereload.listen();
+    gulp.watch("./scss/**/*.scss", gulp.series("sass"));
+    done();
+  })
+);
---
+++ themes/custom/seeds_coat/subtheme/js/main.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.THEMENAME = {
+    attach: function(context, settings) {
+      // Write Your Code Here
+    }
+  };
+
+})(jQuery, Drupal);
