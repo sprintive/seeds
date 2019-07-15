cc33b73fd67684d8c2b8ccbacc3d0c603be3fdb9
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2019-07-15 18:02:31 +0300

    Issue #3054170 by m.abdulqader: Create new major version depend on 8.7.x and Seeds enhancements

--- themes/custom/seeds_coat/scripts/create_subtheme.sh
+++ themes/custom/seeds_coat/scripts/create_subtheme.sh
@@ -1,28 +1,50 @@
 #!/bin/bash
 # Script to quickly create sub-theme.
 
+error_message () {
+    echo -e "\033[41m$1\033[0m\033[K"
+}
+
+# function to check that all requirements exist.
+exit_if_error () {
+    if [ ! $1 -eq 0 ]
+    then
+        error_message " There are errors or warnings detected in your code.\n \033[1mFix The errors then try to generate theme again.$3 "
+		exit 1
+    fi
+}
+
 echo '
 +------------------------------------------------------------------------+
 | With this script you could quickly create seeds_coat sub-theme     |
-| In order to use this:                                                  |
+| In order to use this:                                              |
 | - seeds_coat theme (this folder) should be in the contrib folder   |
 +------------------------------------------------------------------------+
 '
-echo 'The machine name of your custom theme? [e.g. mycustom_seeds_coat]'
-read CUSTOM_BOOTSTRAP_SASS
 
-echo 'Your theme name ? [e.g. My custom seeds_coat]'
-read CUSTOM_BOOTSTRAP_SASS_NAME
+PROJECT_FOLDER="$(git rev-parse --show-toplevel)"; exit_if_error $?;
+PROJECT_ROOT="$PROJECT_FOLDER/public_html";
+SUBTHEME_DIRECTORY="$PROJECT_ROOT/profiles/contrib/seeds/themes/custom/seeds_coat/subtheme"
+THEMES_DIRECTORY="$PROJECT_ROOT/themes";
 
-if [[ ! -e ../../../custom ]]; then
-    mkdir ../../../custom
+# Get theme information.
+echo 'The machine name of your custom theme? [e.g. hello_world]'
+read CUSTOM_SEEDS_THEME
+
+echo 'Your theme name ? [e.g. Hello World]'
+read CUSTOM_SEEDS_THEME_NAME
+
+# Check of themes folder exist. and create if not.
+if [[ ! -e $PROJECT_ROOT/themes ]]; then
+    mkdir $PROJECT_ROOT/themes
 fi
-cd ../../../custom
-cp -r ../contrib/seeds_coat $CUSTOM_BOOTSTRAP_SASS
-cd $CUSTOM_BOOTSTRAP_SASS
-for file in *seeds_coat.*; do mv $file ${file//bootstrap_sass/$CUSTOM_BOOTSTRAP_SASS}; done
-for file in config/*/*seeds_coat.*; do mv $file ${file//bootstrap_sass/$CUSTOM_BOOTSTRAP_SASS}; done
-mv {_,}$CUSTOM_BOOTSTRAP_SASS.theme
-grep -Rl seeds_coat .|xargs sed -i '' -e "s/bootstrap_sass/$CUSTOM_BOOTSTRAP_SASS/"
-sed -i '' -e "s/SASS Bootstrap Starter Kit Subtheme/$CUSTOM_BOOTSTRAP_SASS_NAME/" $CUSTOM_BOOTSTRAP_SASS.info.yml
-echo "# Check the themes/custom folder for your new sub-theme."
+
+# Copy subtheme to the themes folder.
+
+cp -r $SUBTHEME_DIRECTORY $THEMES_DIRECTORY/$CUSTOM_SEEDS_THEME;
+cd $THEMES_DIRECTORY/$CUSTOM_SEEDS_THEME;
+
+for file in *THEMENAME.*; do mv $file ${file//THEMENAME/$CUSTOM_SEEDS_THEME}; done
+for file in config/*/*THEMENAME*; do mv $file ${file//THEMENAME/$CUSTOM_SEEDS_THEME}; done
+
+grep -Rl THEMENAME .|xargs sed -i '' -e "s/THEMENAME/$CUSTOM_SEEDS_THEME/"
