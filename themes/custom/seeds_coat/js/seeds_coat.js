7ad9476011b8689690967c46b2d883287a4e61cd
Mohammad Ali Abdel-Qader <m.abdulqader@1281252.no-reply.drupal.org>
2019-07-25 22:29:57 +0300

    Issue #3054170 by m.abdulqader: Fix contexual links images

--- themes/custom/seeds_coat/js/seeds_coat.js
+++ themes/custom/seeds_coat/js/seeds_coat.js
@@ -10,13 +10,15 @@
 	Drupal.behaviors.seeds_coat = {
 		attach: function (context, settings) {
 			// Node edit tab.
-			$(".tabs").once('seeds-coat-node-edit').click(function () {
+			$(".tabs").once('seeds_coat').click(function () {
 				$(this).children(".nav-tabs.primary").slideToggle('slow', function () { });
 			});
 			// Remove on click when clicking on contextual links.
-			$("*[onclick*='location.href'] div.contextual").on('click', function (event) {
-				event.stopPropagation();
-			});
+			$(window).on('load', function () {
+				$("*[onclick*='location.href'] div.contextual").on('click', function (event) {
+					event.stopPropagation();
+				});
+			})
 		}
 	};
 
