d794e13b352a48d41e4dfd91283e9f017efbeef0
YahyaAlHamad <YahyaAlHamad@3601769.no-reply.drupal.org>
2022-01-24 18:44:11 +0200

    Issue #3259335 by YahyaAlHamad: Drush.yml configurations

--- modules/custom/seeds_pollination/Drush/Commands/PolicyCommands.php
+++ modules/custom/seeds_pollination/Drush/Commands/PolicyCommands.php
@@ -16,6 +16,10 @@ class PolicyCommands extends DrushCommands {
    * @hook validate sql:sync
    */
   public function validate(CommandData $commandData) {
+    if (file_exists('/tmp/.bypass_rsync_warning')) {
+      unlink('/tmp/.bypass_rsync_warning');
+    }
+    file_put_contents('/tmp/.bypass_rsync_warning', time());
     $target = $commandData->input()->getArgument('target');
     $env = explode('.', $target)[1];
     foreach (self::DANGEROUS_ENV as $dangerous_env) {
@@ -34,16 +38,23 @@ class PolicyCommands extends DrushCommands {
    * @hook validate core:rsync
    */
   public function validateRsync(CommandData $commandData) {
+    // We check for .bypass_rsync_warning and the time because using sql-sync from @self to any env can cause infinite loop here.
     $target_folder = $commandData->input()->getArgument('target');
     $target = explode(':', $target_folder)[0];
     $env = explode('.', $target)[1];
     foreach (self::DANGEROUS_ENV as $dangerous_env) {
-      if (strpos($env, $dangerous_env) !== FALSE) {
+      if (strpos($env, $dangerous_env) !== FALSE && !file_exists('/tmp/.bypass_rsync_warning')) {
         $this->io()->writeln('You are about to do a dangerous operation on the env: ' . $env);
         $answer = $this->io()->ask('Write "I Understand" to continue...');
         if ($answer !== 'I Understand') {
           $this->io()->error('Wrong answer!');
           $this->validate($commandData);
+        }
+      } else if (file_exists('/tmp/.bypass_rsync_warning')) {
+        $time = file_get_contents('/tmp/.bypass_rsync_warning');
+        unlink('/tmp/.bypass_rsync_warning');
+        if ($time < (time() - 30 * 60)) {
+          $this->validateRsync($commandData);
         }
       }
     }
@@ -59,9 +70,8 @@ class PolicyCommands extends DrushCommands {
     $project_name = array_pop(explode('/', $root));
     $filename = $project_name . time() . '.sql';
     $this->io()->writeln('Creating a backup first...');
-    shell_exec('drush cr'); 
-    shell_exec('drush sql-dump > /tmp/' . $filename);
-    shell_exec('gzip /tmp/' . $filename);
+    shell_exec('drush cr');
+    shell_exec(sprintf('drush sql-dump > /tmp/%s --gzip', $filename . '.gz'));
     $this->io()->writeln('A backup has been created in /tmp/' . $filename . '.gz');
   }
 
